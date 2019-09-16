3f20bbec273a6c30d0ff9efccf551538e241e677
meetmahajan <monoarchos@gmail.com>
2019-09-16 18:21:59 +0530

    Nested FormGroups -- /reactive-form

--- src/app/reactive-form/reactive-form.component.ts
+++ src/app/reactive-form/reactive-form.component.ts
@@ -15,6 +15,12 @@ export class ReactiveFormComponent implements OnInit {
     email: [''],
     mobile: [''],
     designation: [''],
+    address: this.formbuilder.group({
+      street: [''],
+      city: [''],
+      state: [''],
+      zip: ['']
+    })
   });
   constructor(private formbuilder: FormBuilder) {
   }
